fbe52958a35b9ec5d511a6811fdaa3f77b8a4899
socialbeaver <112391573+socialbeaver@users.noreply.github.com>
2022-12-21 16:53:02 -0700

    pyautogui
    Co-authored-by: SAL778 <saahilrachh1234@gmail.com>

--- type.py
+++ type.py
@@ -1,10 +1,11 @@
 import pyperclip
 import os
+import pyautogui as p
 
 os.system("notepad")
 pyperclip.copy('Please type this out')
 s = pyperclip.paste()
-with open("type.txt","w") as p:
-    p.write(s)
+#with open("type.txt","w") as p:
+#    p.write(s)
     
 #save and close a particular file
